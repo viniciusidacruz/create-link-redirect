2474af502728d874d7b1f0f489205d81ce524e18
Vinicius Italo da Cruz <viniciusidacruz@hotmail.com>
2024-01-01 19:57:09 -0300

    feat: :sparkles: implement rules for redirect
    implement rules for redirect link and create route page of redirect

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 
 application = Flask(__name__)
 
-@application.route("/")
+urls = {}
+
+@application.route("/", methods=["GET", "POST"])
 def home_page():
-    return render_template("homepage.html")
+    if request.method == "POST":
+        default_url = request.form["default_link"];
+        new_path = request.form["new_path"];
+        
+        if new_path not in urls.keys():
+            urls[new_path] = default_url
+            return f"Seu link: {request.url_root}{new_path}"
+        
+        else:
+            return "Caminho Destino não disponível, tente outro"
+        
+    else:
+        return render_template("homepage.html")
     
+@application.route("/<new_path>")
+def redirect(new_path):
+    default_url = urls.get(new_path)
+    
+    if default_url:
+        return redirect(default_url)
+    else:
+        return "Página não encontrada", 404
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    application.run(debug=True) 
